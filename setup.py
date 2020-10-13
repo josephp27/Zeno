3919fda0f6f36e3f93bc2cdefb232c8a3961e80a
Joseph Procopio <josephp27@users.noreply.github.com>
2020-10-13 13:08:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 import os
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+VERSION = '0.' + os.getenv('TRAVIS_BUILD_NUMBER', '0.0')
 
 setup(
     name='ZenoMapper',
     packages=['ZenoMapper'],
     license='Apache license 2.0',
     description='An Object Config Mapper (OCM)',
-    version='0.37.1',
+    version=VERSION,
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Joseph Procopio',
     author_email='josephp27@live.com',
+    packages=find_packages(exclude=['test', 'tests']),
+    options={'bdist_wheel' : {'universal': True}},
     url='https://github.com/josephp27/Zeno',
     keywords=['Zeno', 'OCM', 'Config', 'Configuration', 'Mapper', 'Object'],
     classifiers=[
