9706204cfc8245624b3dc7b2a80619b74cc81a0c
Joseph Procopio <jprocopio@box.com>
2020-10-14 22:10:48 -0500

    checks cwd

--- ZenoMapper/__init__.py
+++ ZenoMapper/__init__.py
@@ -1,5 +1,1 @@
-import os
-
 from ZenoMapper.Types import *
-
-__version__ = '0' + os.environ.get('TRAVIS_BUILD_NUMBER', '0.0')
--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 import os
 
 from setuptools import setup, find_packages
-from ZenoMapper import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+print('!!!!!!!!!!!!!!!!!!!' + os.getcwd())
 setup(
     name='ZenoMapper',
     license='Apache license 2.0',
     description='An Object Config Mapper (OCM)',
-    version=__version__,
+    version='0.' + os.environ.get('TRAVIS_BUILD_NUMBER', '0.0'),
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Joseph Procopio',
     author_email='josephp27@live.com',
     packages=find_packages(exclude=['test', 'tests']),
-    options={'bdist_wheel' : {'universal': True}},
+    options={'bdist_wheel': {'universal': True}},
     url='https://github.com/josephp27/Zeno',
     keywords=['Zeno', 'OCM', 'Config', 'Configuration', 'Mapper', 'Object'],
     classifiers=[
