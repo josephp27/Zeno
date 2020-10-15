a1cdabe1b438b002aa0caf2b6e9548def5554e32
Joseph Procopio <jprocopio@box.com>
2020-10-14 22:50:51 -0500

    checks dirs

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+print("!!!!!!!!!!!!!!!!!!!!!" + os.getcwd())
 with open('__version__', 'r') as fh:
     version = fh.read()
 
