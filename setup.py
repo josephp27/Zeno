60876c8f544330402ddd36e9a84ecc846edf327a
Joseph Procopio <jprocopio@box.com>
2020-10-14 23:18:24 -0500

    adds version to module

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('__version__', 'r') as fh:
+with open('ZenoMapper/version', 'r') as fh:
     version = fh.read()
 
 setup(
