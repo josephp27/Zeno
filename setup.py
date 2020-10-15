a48d60bc58a17e78eb12c8f20f3c9581eafa1249
Joseph Procopio <jprocopio@box.com>
2020-10-14 22:54:33 -0500

    changes to ls

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-print("!!!!!!!!!!!!!!!!!!!!!" + os.getcwd())
+print("!!!!!!!!!!!!!!!!!!!!!" + str(os.listdir()))
 with open('__version__', 'r') as fh:
     version = fh.read()
 
