bf66c83db2a5f8a5c7b8ad3b2fd1f7ccc109a3e4
Joseph Procopio <josephp27@live.com>
2020-12-19 18:53:51 -0600

    fixes version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-from ZenoMapper.version import __version__
+from version import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
